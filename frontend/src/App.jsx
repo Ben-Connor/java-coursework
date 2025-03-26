0a1d7cd66d19ee994f4b2c44065a0bf81ef88cf3
GreevesW <wkgreeves@gmail.com>
2025-03-26 01:14:55 +0000

    Added food search page using OpenFoodFacts API. Modified routing.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,12 @@
 import { Link, Outlet } from '@tanstack/react-router';
+import FoodSearch from './FoodSearch';
 
 function App() {
   return (
     <div>
       <nav>
-        <Link to="/">Home</Link>
+        {/* <Link to="/">Home</Link> */}
+        <a href="/">Home</a> | <a href="/food-search">Food Search</a>
       </nav>
       <hr />
       <Outlet /> {}
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,6 +1,7 @@
 import { createRootRoute, createRoute, RouterProvider, createRouter } from '@tanstack/react-router';
 import Home from './pages/Home';
 import App from './App';
+import FoodSearch from './FoodSearch';
 
 const rootRoute = createRootRoute({
   component: App,
@@ -11,7 +12,14 @@ const homeRoute = createRoute({
   path: '/',
   component: Home,
 });
-const routeTree = rootRoute.addChildren([homeRoute]);
+
+const foodSearchRoute = createRoute({
+  getParentRoute: () => rootRoute, // It's a child of the root route
+  path: '/food-search', // The path that will render the FoodSearch component
+  component: FoodSearch, // FoodSearch component will render here
+});
+
+const routeTree = rootRoute.addChildren([homeRoute, foodSearchRoute]);
 
 const router = createRouter({ routeTree });
 
